9010b20503d2ffb8849aeb500abda7b0a7a042a0
謝政宏 <b3194359@gmail.com>
2020-04-17 23:40:08 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+//地圖組建
+import Map from './components/map';
+
+function App() {
+  return (
+    <>
+      <Map />
+    </>
+  );
+}
+
+export default App;
