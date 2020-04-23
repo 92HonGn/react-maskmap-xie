e566325053ad757f7b014f0ac4000925b16c4d9c
謝政宏 <b3194359@gmail.com>
2020-04-23 21:59:09 +0800

    新增SearchList搜尋結果顯示組件

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 //地圖組建
-import Map from './components/Viewport';
+import Map from './components/Search';
+import ShowList from './components/ShowList';
 
 export default function App() {
   return (
+     <>
       <Map />
+      <ShowList />
+     </>
   );
 }
 
---
+++ src/components/ShowList.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+export default function ShowList() {
+
+  return (
+    <div>
+      <ul>
+        <li>
+          <div className="store-name">
+            <h3>XXX藥局</h3>
+            <p>
+              <span>營業中</span>
+              <span>300m</span>
+            </p>
+          </div>
+          <div className="store-intro">
+            <div className="address">
+              <span>icon</span>
+              <span>地址</span>
+            </div>
+            <div className="phone">
+              <span>icon</span>
+              <span>電話</span>
+            </div>
+            <div className="time">
+              <span>icon</span>
+              <span>07:00-9:00,</span>
+              <span>13:00-20:00</span>
+            </div>
+          </div>
+          <div className="store-mask">
+            <div className="adult">
+              成人口罩<span>11</span>個
+            </div>
+            <div className="child">
+              兒童口罩<span>123</span>個
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
