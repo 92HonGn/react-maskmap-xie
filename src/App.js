4b5747bb0f694501ccaab9f491efd3b45054fcae
謝政宏 <b3194359@gmail.com>
2020-05-07 01:32:09 +0800

    改寫

--- src/App.js
+++ src/App.js
@@ -1,15 +1,91 @@
-import React from 'react'
-
+import React,{ useState, useEffect } from 'react'
 import Sidebar from './pages/Sidebar'
 import MaskMap from './pages/MaskMap'
 import './App.css';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [localdata, setLocalData] = useState([]);
+  const [nowlongitude , setNowlongitude] = useState();
+  const [nowlatitude , setNowlatitude] = useState();
+  const {value, bind, reset} = useInput('');
+
+  const apiURL = "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json?fbclid=IwAR1k5dAvUSR7XCoG_H_RQx9pzYyJEMqG9AN06e4HNJIASIv-_gwTseX4sSI";
+
+  const distance = 800;
+
+  const space = (latitude1, longitude1, latitude2, longitude2) => {
+    let radLat1 = latitude1 * Math.PI / 180.0;
+    let radLat2 = latitude2 * Math.PI / 180.0;
+    let a = radLat1 - radLat2;
+    let b = longitude1 * Math.PI / 180.0 - longitude2 * Math.PI / 180.0;
+    let s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+    s = s * 6378.137;
+    s = Math.round(s * 10000) / 10000 * 1000;
+    return s //單位公尺
+  }
+
+  const handlePosition = () =>  {
+    if(navigator.geolocation) {
+      function error() {
+        alert('無法取得你的位置')
+      }
+      function success(position) {
+        console.log(`經度:${position.coords.longitude}, 緯度:${position.coords.latitude}`);
+        setNowlongitude(position.coords.longitude);
+        setNowlatitude(position.coords.latitude);
+      }
+      navigator.geolocation.getCurrentPosition(success, error)
+    } else {
+      alert('Sorry, 你的裝置不支援地理位置功能。')
+    }
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert(`Submitting Name ${value}`);
+    reset();
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await fetch(apiURL);
+      let responseData  = await response.json();
+      if(nowlongitude && nowlatitude){
+        let filteredData = responseData.features.filter(item => {
+          return space(nowlatitude, nowlongitude, item.geometry.coordinates[1], item.geometry.coordinates[0]) < distance;
+        });
+        setLocalData(filteredData);
+      }else{
+        let filteredData = responseData.features.filter(item => {
+          return item.properties.address.includes(value);
+        });
+        setData(filteredData);
+      }
+    };
+    fetchData();
+  },[value, nowlongitude, nowlatitude]);
+
   return (
     <>
-      <Sidebar />
+      <Sidebar location={handlePosition} submit={handleSubmit} Bind={ bind } initData={data} localData={localdata}/>
       <MaskMap />
     </>
   );
 }
+
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  return {
+    value,
+    setValue,
+    reset: () => setValue(""),
+    bind: {
+      value,
+      onChange: event => {
+        setValue(event.target.value);
+      }
+    }
+  };
+};
 
--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -1,66 +1,24 @@
-import React,{ useState } from "react";
+import React from "react";
 
 //側邊欄
 import Search from '../components/Search';
 import Location from '../components/location';
 import Filter from '../components/filter';
-
 import ShowList from '../components/ShowList';
 
-export default function Sidebar() {
-  const { value, bind, reset } = useInput('');
-  const [nowlongitude , setNowlongitude] = useState();
-  const [nowlatitude , setNowlatitude] = useState();
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    alert(`Submitting Name ${value}`);
-    reset();
-  }
-
-  const handlePosition = () =>  {
-    if(navigator.geolocation) {
-      // 使用者不提供權限，或是發生其它錯誤
-      function error() {
-        alert('無法取得你的位置')
-      }
-      // 使用者允許抓目前位置，回傳經緯度
-      function success(position) {
-        console.log(`經度:${position.coords.longitude}, 緯度:${position.coords.latitude}`);
-        setNowlongitude(position.coords.longitude);
-        setNowlatitude(position.coords.latitude);
-      }
-      // 跟使用者拿所在位置的權限
-      navigator.geolocation.getCurrentPosition(success, error)
-    } else {
-      alert('Sorry, 你的裝置不支援地理位置功能。')
-    }
-  }
-
+export default function Sidebar({location, submit, Bind, initData, localData}) {
+  
   return (
      <div className="sidebar-component">
        <div className="search-components">
-          <Search onSubmitValue={ handleSubmit } bindValue={ bind }/>
-          <Location onHandlePostion={handlePosition} />
+          <Search onSubmitValue={ submit } bindValue={ Bind }/>
+          <Location onHandlePostion={location} />
           <Filter/>
        </div>
-       <ShowList storeName={value} currentlongitude={nowlongitude} currentlatitude={nowlatitude}/>
+         <ShowList data={localData}/>
      </div>
   );
 }
 
-const useInput = initialValue => {
-  const [value, setValue] = useState(initialValue);
-  return {
-    value,
-    setValue,
-    reset: () => setValue(""),
-    bind: {
-      value,
-      onChange: event => {
-        setValue(event.target.value);
-      }
-    }
-  };
-};
+
 
